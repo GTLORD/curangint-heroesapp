7eed4779411217bf4cb148e0cf307400bea7b4d6
Therry <gtlord.50@gmail.com>
2024-03-27 11:15:14 -0500

    Primer commit curso angular heroes app

---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,49 @@
+import { inject } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  CanMatchFn,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+const checkAuthNoLogin = (): boolean | Observable<boolean> => {
+  //se inyectan el AuthService y el Router
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  return authService.checkAuthentication()
+  .pipe(
+    tap((isAuthenticated) => {
+      if (isAuthenticated) {
+        router.navigate(['/']);
+      }
+    }),
+    map( isAutenticated => !isAutenticated  )
+  )
+}
+export const publicActivateGuard: CanActivateFn = (
+  //Hay que tener en cuenta el tipado CanActiveFn
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  console.log('CanActivate');
+  console.log({ route, state });
+
+  return checkAuthNoLogin();
+};
+
+export const publicMatchGuard: CanMatchFn = (
+  //Tipado CanMatchFN
+  route: Route,
+  segments: UrlSegment[]
+) => {
+  console.log('CanMatch');
+  console.log({ route, segments });
+
+  return checkAuthNoLogin();
+};
